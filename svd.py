45eccfd56ed12d206bdeec8e3ac915ca0b9f50e9
nikolai0188 <n.yurttaguel@gmx.net>
2016-10-26 15:31:00 +0200

    Update svd.py

--- svd.py
+++ svd.py
@@ -6,10 +6,6 @@ from scipy import misc
 
 A = misc.imread('face.png')
 
-print(A)
-
-A = [[3, 5, 1],[6, 2, 10],[4, 3, 7]]
-
 A_SYM = np.dot(np.transpose(A),A)
 
 D,U = LA.eig(A_SYM)
@@ -19,7 +15,3 @@ D = np.dot(D,D)
 A_SYM = np.dot(A,np.transpose(A))
 
 D,V = LA.eig(A_SYM)
-
-print(A)
-print(U)
-print(V)
