699b7090d8d8f14116446f66bb773b9ca07823a6
nikolai0188 <n.yurttaguel@gmx.net>
2016-10-26 15:55:46 +0200

    Update svd.py

--- svd.py
+++ svd.py
@@ -4,9 +4,13 @@ import matplotlib as mp
 import scipy
 from scipy import misc
 
-A = misc.imread('face.png')
+#A = misc.imread('face.png')
 
-A = A[1,:,:]
+#A_R = A[1,:,:]
+#A_G = A[2,:,:]
+#A_B = A[3,:,:]
+
+A = [[4, 6, 2],[12, 5, 8],[3, 5, 14]]
 
 A_SYM = np.dot(np.transpose(A),A)
 
@@ -17,3 +21,5 @@ D = np.dot(D,D)
 A_SYM = np.dot(A,np.transpose(A))
 
 D,V = LA.eig(A_SYM)
+
+print(D)
