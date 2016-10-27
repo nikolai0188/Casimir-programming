3c56fa00ec419df2ea0d3876381472ffcc12f0aa
nikolai0188 <n.yurttaguel@gmx.net>
2016-10-27 16:16:28 +0200

    Update svd.py

--- svd.py
+++ svd.py
@@ -1,28 +1,47 @@
-import numpy
-from numpy import linalg
+import numpy as np
+from numpy import linalg as LA
+from numpy import random
 import matplotlib as mp
+import matplotlib.image as mpimg
 import scipy
+from scipy import linalg as LA2
 from scipy import misc
+import matplotlib.pyplot as plt
+%matplotlib inline
 
-#A = misc.imread('face.png')
+img = mpimg.imread('face.png')
+B = np.zeros(img.shape)
 
-#A_R = A[1,:,:]
-#A_G = A[2,:,:]
-#A_B = A[3,:,:]
+for i in range(3):
 
-A = [[4, 6, 2],[12, 5, 8],[3, 5, 14]]
+    A = img[:,:,i]
+    
+    #A_SYM = numpy.dot(numpy.transpose(A),A)
 
-A_SYM = numpy.dot(numpy.transpose(A),A)
+    #D,U = linalg.eig(A_SYM)
 
-D,U = linalg.eig(A_SYM)
+    #D = numpy.dot(D,D)
 
-D = numpy.dot(D,D)
+    #A_SYM = numpy.dot(A,numpy.transpose(A))
 
-A_SYM = numpy.dot(A,numpy.transpose(A))
+    #D,V = linalg.eig(A_SYM)
 
-D,V = linalg.eig(A_SYM)
+    #rows = 10
+    #columns = 6
+    #A = random.randint(0,256,size=(rows,columns))
+    #print(A)
 
-print(A)
-print(U)
-print(V)
-print(D)
+    compression = 600
+
+    U,s,V = LA2.svd(A,full_matrices=False)
+
+    s=s[:-compression]
+
+    S = np.diag(s)
+
+    U = np.delete(U,np.s_[U.shape[1]-compression:],1)
+    V = np.delete(V,np.s_[V.shape[0]-compression:],0)
+
+    B[:,:,i] = np.dot(U,np.dot(S,V))
+
+plt.imshow(B)
